175d7e6728e2fb62f4f17b5e88935ced4deee415
AngeliniAxel <axel.angelini@gmail.com>
2025-05-31 11:22:54 +0200

    add ui controls

--- src/app/components/VideoPlayer.tsx
+++ src/app/components/VideoPlayer.tsx
@@ -1,18 +1,50 @@
 'use client';
 
 import React, { useEffect, useRef } from 'react';
-import shaka from 'shaka-player';
+import shaka from 'shaka-player/dist/shaka-player.ui';
 import 'shaka-player/dist/controls.css';
+import './VideoPlayer.css';
 
 const VideoPlayer = () => {
     const videoRef = useRef<HTMLVideoElement | null>(null);
+    const containerRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
+        if (!videoRef.current || !containerRef.current) return;
+        shaka.polyfill.installAll();
+
         const player = new shaka.Player(videoRef.current);
+        const ui = new shaka.ui.Overlay(player, containerRef.current, videoRef.current);
+
+        const config = {
+            seekBarColors: {
+                base: 'rgba(255, 255, 255, 0.20)',
+                buffered: 'rgba(255,255,255,.4)',
+                played: '#d44338',
+            },
+            controlPanelElements: [
+                'play_pause',
+                'rewind',
+                'fast_forward',
+                'mute',
+                'volume',
+                'spacer',
+                'time_and_duration',
+                'fullscreen',
+                'overflow_menu',
+            ],
+            overflowMenuButtons: ['quality', 'playback_rate'],
+        };
+        ui.configure(config);
 
         player.load('https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd');
     }, []);
-    return <video ref={videoRef} controls />;
+
+    return (
+        <div className='w-xl h-auto' ref={containerRef}>
+            <video ref={videoRef} />
+        </div>
+    );
 };
 
 export default VideoPlayer;
