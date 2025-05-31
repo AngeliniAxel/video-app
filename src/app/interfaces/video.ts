5616d7c135abde4e63ae7a6404d40bf9760869d0
AngeliniAxel <axel.angelini@gmail.com>
2025-05-31 17:33:43 +0200

    update videos.json

---
+++ src/app/interfaces/video.ts
@@ -0,0 +1,12 @@
+export interface Video {
+    id: string;
+    title: string;
+    thumbnailUrl: string;
+    duration: string;
+    uploadTime: string;
+    views: string;
+    author: string;
+    videoUrl: string;
+    description: string;
+    subscriber: string;
+}
