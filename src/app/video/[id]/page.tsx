305055dec919e37c84439cd26dbccceeffb71bb6
AngeliniAxel <axel.angelini@gmail.com>
2025-06-01 13:51:52 +0200

    styles to scroll bar

--- src/app/video/[id]/page.tsx
+++ src/app/video/[id]/page.tsx
@@ -5,6 +5,7 @@ import { Video } from '../../interfaces/video';
 import axios from 'axios';
 import dynamic from 'next/dynamic';
 import React, { useEffect, useState } from 'react';
+import './page.css';
 
 const VideoPlayer = dynamic(() => import('../../components/VideoPlayer/VideoPlayer'), {
     ssr: false,
@@ -63,7 +64,7 @@ const Page = ({ params }: Props) => {
                         </div>
                     )}
                 </div>
-                <div className='w-full flex flex-col flex-wrap lg:flex-row items-center gap-8 lg:w-[380px] h-auto lg:h-[100vh] lg:overflow-y-auto py-1 overflow-hidden'>
+                <div className='w-full flex flex-col flex-wrap lg:flex-row items-center gap-8 lg:w-[380px] h-auto lg:h-[100vh] lg:overflow-y-auto py-1 overflow-hidden scrollbar'>
                     {unselectedVideos?.map((video: Video) => (
                         <VideoCard key={video.id} video={video} />
                     ))}
